6356acf76033bc6b606a296b285de2c2fb540b91
katznboyz1 <harrison.lindgren@gmail.com>
2021-11-26 20:05:46 -0500

    more code structure changes

--- graphics_output_test.ino
+++ graphics_output_test.ino
@@ -1,6 +1,7 @@
 /*
     I got my instructions on how to make the GPU from this video: https://youtu.be/l7rce6IQDWs
     Signal timings are from http://tinyvga.com/vga-timing
+    https://tomverbeure.github.io/video_timings_calculator (400x300)
 */
 
 #include <Arduino.h>
@@ -8,7 +9,7 @@
 
 using namespace std;
 
-struct VGA {
+struct VGA_400x300 {
     const int PIN_RED = 8;
     const int PIN_GREEN = 9;
     const int PIN_BLUE = 10;
@@ -17,17 +18,20 @@ struct VGA {
     const int PIN_VERTICAL_SYNC = 12;
 
     // x, y, hz
-    const int OUTPUT_RESOLUTION[3] = {160, 120, 60};
+    const int OUTPUT_RESOLUTION[3] = {400, 300, 60};
+    const int OUTPUT_RESOLUTION_TOTAL[2] = {496, 324};
 
     // min, max
     const int OUTPUT_MIN_MAX_ANALOG_SIGNAL = 255;
 
-    // microseconds
-    const double OUTPUT_PERIOD_HORIZONTAL = (1 / 60) * 1e6;
-    const double OUTPUT_PERIOD_VERTICAL = (1 / 60) * 1e6;
+    const double VERTICAL_SYNC_kHz = 19.153;
+    const double VERTICAL_SYNC_Hz = 59.115;
+
+    volatile int x, y = 0;
+    volatile int r, g, b = 1;
 };
 
-VGA VGAController;
+VGA_400x300 VGAController;
 
 void setup() {
 
@@ -42,7 +46,7 @@ void setup() {
 
 void loop() {
 
-    analogWrite(VGAController.PIN_RED, VGAController.OUTPUT_MIN_MAX_ANALOG_SIGNAL);
-    analogWrite(VGAController.PIN_GREEN, VGAController.OUTPUT_MIN_MAX_ANALOG_SIGNAL);
-    analogWrite(VGAController.PIN_BLUE, VGAController.OUTPUT_MIN_MAX_ANALOG_SIGNAL);
+    analogWrite(VGAController.PIN_RED, VGAController.OUTPUT_MIN_MAX_ANALOG_SIGNAL * VGAController.r);
+    analogWrite(VGAController.PIN_GREEN, VGAController.OUTPUT_MIN_MAX_ANALOG_SIGNAL * VGAController.g);
+    analogWrite(VGAController.PIN_BLUE, VGAController.OUTPUT_MIN_MAX_ANALOG_SIGNAL * VGAController.b);
 }
