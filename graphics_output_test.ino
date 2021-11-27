e7e5737c6f330bd615952974d82b5a8b57f6b855
katznboyz1 <harrison.lindgren@gmail.com>
2021-11-26 20:15:05 -0500

    made data type changes and added two vga_... functions

--- graphics_output_test.ino
+++ graphics_output_test.ino
@@ -5,16 +5,17 @@
 */
 
 #include <Arduino.h>
+#include <Wire.h>
 
 using namespace std;
 
 struct VGA_400x300 {
-    const int PIN_RED = 8;
-    const int PIN_GREEN = 9;
-    const int PIN_BLUE = 10;
+    const byte PIN_RED = 8;
+    const byte PIN_GREEN = 9;
+    const byte PIN_BLUE = 10;
 
-    const int PIN_HORIZONTAL_SYNC = 11;
-    const int PIN_VERTICAL_SYNC = 12;
+    const byte PIN_HORIZONTAL_SYNC = 11;
+    const byte PIN_VERTICAL_SYNC = 12;
 
     // x, y, hz
     const int OUTPUT_RESOLUTION[3] = {400, 300, 60};
@@ -27,10 +28,28 @@ struct VGA_400x300 {
     const double VERTICAL_SYNC_Hz = 59.115;
 
     volatile int x, y = 0;
-    volatile int r, g, b = 1;
+    volatile byte r = 0;
+    volatile byte g = 1;
+    volatile byte b = 0;
+
+    bool is_initialized = false;
 };
 
 VGA_400x300 VGAController;
+
+void vga_sleep() {
+
+    if (VGAController.is_initialized) {
+
+    }
+}
+
+void vga_scanline() {
+
+    if (VGAController.is_initialized) {
+        
+    }
+}
 
 void setup() {
 
@@ -41,11 +60,12 @@ void setup() {
     pinMode(VGAController.PIN_BLUE, OUTPUT);
     pinMode(VGAController.PIN_HORIZONTAL_SYNC, OUTPUT);
     pinMode(VGAController.PIN_VERTICAL_SYNC, OUTPUT);
+
+    VGAController.is_initialized = true;
 }
 
 void loop() {
 
-    analogWrite(VGAController.PIN_RED, VGAController.OUTPUT_MIN_MAX_ANALOG_SIGNAL * VGAController.r);
-    analogWrite(VGAController.PIN_GREEN, VGAController.OUTPUT_MIN_MAX_ANALOG_SIGNAL * VGAController.g);
-    analogWrite(VGAController.PIN_BLUE, VGAController.OUTPUT_MIN_MAX_ANALOG_SIGNAL * VGAController.b);
+    vga_sleep();
+    vga_scanline();
 }
